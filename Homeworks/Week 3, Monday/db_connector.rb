9ccbc968443a8d9bf8de6a83898c8b89a1f3aefb
Hafidza Hadi <hhad0002@student.monash.edu>
2021-07-21 16:54:05 +0700

    Homework 5: 19/07/2021

---
+++ Homeworks/Week 3, Monday/db_connector.rb
@@ -0,0 +1,98 @@
+require 'mysql2'
+require './item'
+require './category'
+
+# Initialize DB
+def create_db_client
+    client = Mysql2::Client.new(
+        :host => "localhost",
+        :username => "root",
+        :password => "Haqqutata06",
+        :database => "food_oms_db"
+    )
+
+    client
+end
+
+#Example
+def get_all_items
+    client = create_db_client
+    rawData = client.query("SELECT * FROM items")
+
+    items = Array.new
+
+    rawData.each do | data |
+        item = Item.new(data["id"], data["name"], data["price"])
+        items.push(item)
+    end
+
+    items
+end
+
+def create_new_item(name, price, category_name = nil)
+    client = create_db_client
+    client.query("INSERT INTO items(name, price) VALUES('#{name}', '#{price}')")
+
+    item_data = client.query("SELECT id FROM items WHERE name = '#{name}'")
+    item_id = nil
+    item_data.each do | data |
+        item_id = data["id"]
+    end
+
+    if category_name != "None"
+        category_data = client.query("SELECT id FROM categories WHERE name = '#{category_name}'")
+        category_id = nil
+        category_data.each do | data |
+            category_id = data["id"]
+        end
+
+        client.query("INSERT INTO item_categories(item_id, category_id) VALUES(#{item_id}, #{category_id})")
+    end
+end
+
+def get_item_with_id(id)
+    client = create_db_client
+    rawData = client.query("
+        SELECT i.*, c.id AS 'category_id', c.name AS 'category_name'
+        FROM items i
+        LEFT JOIN item_categories ic ON i.id = ic.item_id
+        LEFT JOIN categories c ON ic.category_id = c.id
+        WHERE i.id = #{id}
+    ")
+    puts(rawData.each)
+
+    item = nil
+
+    rawData.each do | data |
+        category = Category.new(data["category_id"], data["category_name"])
+        item = Item.new(data["id"], data["name"], data["price"], category)
+    end
+
+    item
+end
+
+def delete_item_with_id(id)
+    client = create_db_client
+
+    client.query("DELETE FROM item_categories WHERE item_id = #{id}")
+    client.query("DELETE FROM items WHERE id = #{id}")
+end
+
+def edit_item_with_id(id, name, price, category_name)
+    client = create_db_client
+    client.query("UPDATE items SET name = '#{name}', price = #{price} WHERE id = #{id}")
+
+    client.query("DELETE FROM item_categories WHERE item_id = #{id}")
+
+    if category_name != "None"
+        category_data = client.query("SELECT id FROM categories WHERE name = '#{category_name}'")
+        category_id = nil
+        category_data.each do | data |
+            category_id = data["id"]
+        end
+
+        client.query("INSERT INTO item_categories(item_id, category_id) VALUES(#{id}, #{category_id})")
+    end
+end
+
+items = get_all_items
---
+++ Homeworks/Week 3, Monday/router.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require './db_connector'
+
+get '/' do
+    items = get_all_items
+    erb :index, locals: {
+        items: items
+    }
+end
+
+get '/items/new' do
+    erb :create_item
+end
+
+get '/items/edit/:id' do
+    id = params['id']
+    item = get_item_with_id(id)
+
+    erb :edit_item, locals: {
+      item: item
+    }
+end
+
+get '/items/details/:id' do
+    id = params['id']
+    item = get_item_with_id(id)
+
+    erb :item_details, locals: {
+      item: item
+    }
+end
+
+get '/items/delete/:id' do
+    id = params['id']
+    delete_item_with_id(id)
+
+    redirect '/'
+end
+
+post '/items/create' do
+    name = params['name']
+    price = params['price']
+    category_name = params['category_name']
+
+    create_new_item(name, price, category_name)
+    redirect '/'
+end
+
+post '/items/edit/:id' do
+    id = params['id']
+    name = params['name']
+    price = params['price']
+    category_name = params['category_name']
+
+    edit_item_with_id(id, name, price, category_name)
+    redirect '/'
+end
